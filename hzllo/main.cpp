e96f3aaf0b662147a5b2fe63f378c7c0f0934313
Guillaume Lazaro <guillaume.lazaro@webinage.fr>
2020-07-05 15:53:03 +0200

    clean main.cpp

--- hzllo/main.cpp
+++ hzllo/main.cpp
@@ -19,12 +19,9 @@ using namespace std;
 #include "pokedex.hpp"
 #include "foncTournois.hpp"
 
-int main()
-{
+int main() {
 
     //initialisation var et obj
-  
-    
     Monde monde;
     Joueur player;
     Canne canne;
@@ -41,133 +38,93 @@ int main()
     string nomJoueur;
     
     int choiAction=0;
-    
     int goldJ=0;
     
-    //     intro
-//
+    // intro
     introTexte();
     
-    
-    
-    //     creation monde
-    
-//
+    // creation monde
     creatMondeTexte();
     cin >> nomMonde;
     monde.addNomMonde(nomMonde);
     
-    
-    
-    //     creation joueur
-    
-//
+    //creation joueur
     creatJoueurtexte();
     cin >> nomJoueur;
     player.addNomJoueur(nomJoueur);
-//
     creatJoueurtexteFin();
     
-    
-    
-    
     //Debut du jeux !!!
-    
     bool exit=false;
-    
-    while(exit!=true)
-        {
-            iValid = 1 ;
-            
-            mTempo=player.getMuse();
-            mTempo.modifGoldJourTotal();
-            goldJ=mTempo.getGoldJourTotal();
-            player.addGold(goldJ);
-            
-            
-            monde.upJour();
-            monde.randMeteo();
-            monde.changeJTournois();
-            
-            texteMenu(monde,player);
+    while(exit!=true) {
+        iValid = 1 ;
+        
+        mTempo=player.getMuse();
+        mTempo.modifGoldJourTotal();
+        goldJ=mTempo.getGoldJourTotal();
+        player.addGold(goldJ);
+        
+        
+        monde.upJour();
+        monde.randMeteo();
+        monde.changeJTournois();
+        
+        texteMenu(monde,player);
 
-                   
-                cin>>choiAction;
-
-            
-                iValid = 1 ;
-                while (iValid == 1)
-                {
-                        if (cin.fail())
-                        {
-                                cin.clear();
-                                cin.ignore();
-                                cout<<"Erreur!"<<endl;
-                                cin>>choiAction;
-                        }
-                        else
-                                iValid = 0;
-                }
-                       
-          
-            
-                switch (choiAction)
-                {
-                    case 1:
-                    {
-                        int t=0;
-                        player=combat(player,t,monde);
-                    break;
-                    }
-                    
-                        
-                    case 2:
-                    {
-                        pokedex(player);
-                    break;
-                    }
-                    
-                        
-                    case 3:
-                    {
-                        player=magasin(player);
-                    break;
-                    }
                 
-                        
-                    case 4:
-                    {
-                        mTempo.showMuse();
-                    break;
-                    }
-                        
-                        
-                    case 5:
-                    {
-                        eventTournois (monde, player);
-                    break;
-                    }
-                        
-                    case 6:
-                        exit=true;
-                    break;
-                    
-                    
-                    default:
-                    break;
-                    
-                }
+        cin>>choiAction;
+
+        
+        iValid = 1 ;
+        while (iValid == 1) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore();
+                cout<<"Erreur!"<<endl;
+                cin>>choiAction;
+            } else {
+                iValid = 0;
+            }
+        }
+        
+        switch (choiAction) {
+            case 1: {
+                int t=0;
+                player=combat(player,t,monde);
+                break;
+            }
+                
+            case 2: {
+                pokedex(player);
+                break;
+            }
+                
+            case 3: {
+                player=magasin(player);
+                break;
+            }
+                
+            case 4: {
+                mTempo.showMuse();
+                break;
+            }
+                
+            case 5: {
+                eventTournois (monde, player);
+                break;
+            }
+                
+            case 6:
+                exit=true;
+                break;
             
+            default:
+                break;
             
         }
-    
-    
+    }
     
     //Texte de sortie + fermeture programme
-    
-    
-    
     exittexte();
-    
     return 0;
 }
